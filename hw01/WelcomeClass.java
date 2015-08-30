d4ce7c66278ce72a97e1795faeac365e4d13ae2b
sak219 <sak219@lehigh.edu>
2015-08-30 20:34:01 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Sam Kiernan
+//hw01
+//Welcome Class Java Program
+//August 30 2015
+//Professor Eric Fouh
+//Class
+public class WelcomeClass{
+    //Main Method
+    public static void main(String[] args){
+
+//Body of the Program
+System.out.println("  -----------");
+System.out.println("  | WELCOME |");
+System.out.println("  -----------");
+System.out.println("  ^  ^  ^  ^  ^  ^");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+System.out.println("|-S--A--K--2--1--9-|");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println("  v  v  v  v  v  v");
+//Individual Message
+System.out.println("Both of my parents are immigrants from Ireland,");
+System.out.println("and each summer I go there to play golf with my dad.");
+
+}
+}
